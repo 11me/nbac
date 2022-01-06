c0628a1cf314d413d7a99d627bf4d1fe361beb0d
lime <lime>
2022-01-06 13:26:23 +0300

    use proxy to fetch data

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,28 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
+import FeedsPage from '../views/FeedsPage.vue';
+import OnBoard from '../views/OnBoard.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/feeds'
   },
   {
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/feeds',
+    name: 'Feeds',
+    component: FeedsPage
+  },
+  {
+    path: '/onboard',
+    name: 'OnBoard',
+    component: OnBoard
   }
 ]
 
---
+++ src/views/FeedsPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-page>
+
+    <ion-content :fullscreen="true">
+      <h1>nbac</h1>
+      <ul>
+        <li v-for="feed in feeds" :key="feed.title">{{ feed.title }}</li>
+      </ul>
+    </ion-content>
+
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonPage } from '@ionic/vue';
+import { defineComponent, onMounted } from 'vue';
+import { useFeeds } from '../services/feeds.service';
+
+export default defineComponent({
+  components: {
+    IonContent,
+    IonPage
+  },
+
+  setup() {
+    const { feeds, getFeeds } = useFeeds();
+    return {
+      feeds
+    }
+  }
+});
+</script>
+
+<style>
+</style>
