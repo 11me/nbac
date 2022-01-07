6f549aedf5a88afa644157434414e361e3dd7608
lime <lime>
2022-01-07 21:59:17 +0300

    test get request on android

--- src/views/FeedsPage.vue
+++ src/views/FeedsPage.vue
@@ -5,6 +5,8 @@
       <h1>nbac</h1>
       <ul>
         <li v-for="feed in feeds" :key="feed.title">{{ feed.title }}</li>
+        <p>custom text</p>
+        <ion-button @click="() => getFeeds(thehackernews)">Get</ion-button>
       </ul>
     </ion-content>
 
@@ -12,20 +14,26 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonPage } from '@ionic/vue';
+import { IonContent, IonPage, IonButton } from '@ionic/vue';
 import { defineComponent, onMounted } from 'vue';
 import { useFeeds } from '../services/feeds.service';
+import { sources } from '@11me/xparse';
 
 export default defineComponent({
   components: {
     IonContent,
-    IonPage
+    IonPage,
+    IonButton
   },
 
   setup() {
     const { feeds, getFeeds } = useFeeds();
+    const thehackernews = sources.thehackernews;
+    thehackernews.description.url = 'http://70.34.217.128:8888/thehackernews.com';
     return {
-      feeds
+      feeds,
+      getFeeds,
+      thehackernews
     }
   }
 });
