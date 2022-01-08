c80641b3006f7167675a3f32a590c1974559a64f
nikopushkin <nikopushkin1@gmail.com>
2022-01-08 19:11:20 +0300

    platform initialization fix

--- src/services/session.ts
+++ src/services/session.ts
@@ -4,25 +4,29 @@ import { Capacitor } from '@capacitor/core';
 
 export default class SQLiteSession {
   dbName: string;
-  sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite);
+  sqlite: SQLiteConnection;
+  initializedWeb: boolean;
 
   constructor(dbName: string) {
+    this.sqlite = new SQLiteConnection(CapacitorSQLite)
     this.dbName = dbName;
+    this.initializedWeb = false;
   }
 
   private async initWeb() {
+    // TODO: add IOS and Android initialization
     const platform = Capacitor.getPlatform();
-    if(platform === "web") {
+    if(platform === "web" && !this.initializedWeb) {
       const jeepSqlite = document.createElement('jeep-sqlite');
       document.body.appendChild(jeepSqlite);
       await customElements.whenDefined('jeep-sqlite');
       await this.sqlite.initWebStore();
     }
+    this.initializedWeb = true;
   }
 
   private async conn(dbName: string): Promise<SQLiteDBConnection> {
     await this.initWeb();
-
     const ret = await this.sqlite.checkConnectionsConsistency();
     const isConn = (await this.sqlite.isConnection(dbName)).result;
     let db: SQLiteDBConnection;
