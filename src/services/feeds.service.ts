39153895bd2c31c4d6976af3d090f8e70df24c73
lime <lime>
2022-01-08 19:09:50 +0300

    create models

--- src/services/feeds.service.ts
+++ src/services/feeds.service.ts
@@ -1,39 +1,8 @@
-import { HTMLParser, Options, Feed, sources } from "@11me/xparse";
+import { HTMLParser, Options, sources, Feed } from "@11me/xparse";
 import { Http } from '@capacitor-community/http';
 import { ref } from "vue";
-import { Resource } from '../models'
 
 const feeds = ref<Feed[]>();
-
-async function retrieveResourcesFromDB(): Promise<Resource[]> {
-
-  return [
-    {
-      id: 1,
-      guid: 'guid1',
-      url: 'https://example.com',
-      title: 'News title',
-      author: 'Luke',
-      pub_date: 'Fri, 11 Dec 2020 18:02:48 -0500',
-      content: 'This is some long content of the feed',
-      last_update: 'Fri, 11 Dec 2020 18:02:48 -0500',
-      notify: true,
-      state: true
-    },
-    {
-      id: 2,
-      guid: 'guid2',
-      url: 'https://example.com',
-      title: 'News title 2',
-      author: 'Lime',
-      pub_date: 'Fri, 11 Dec 2020 18:02:48 -0500',
-      content: 'This is some long content of the feed',
-      last_update: 'Fri, 11 Dec 2020 18:02:48 -0500',
-      notify: true,
-      state: true
-    },
-  ]
-}
 
 async function fetchProvider(url: string) {
   const resource = await Http.get({url})
