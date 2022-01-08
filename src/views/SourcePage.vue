133195f6db574c6a37badbd24cd05d98da5fed53
nikopushkin <nikopushkin1@gmail.com>
2022-01-08 16:51:13 +0300

    remove double quotes

--- src/views/SourcePage.vue
+++ src/views/SourcePage.vue
@@ -1,42 +1,42 @@
 <template>
   <ion-page style='padding-right:5%; padding-top: 14%'>
-    <h1 style="margin: 0; margin-left: 8%">Sources</h1>
+    <h1 style='margin: 0; margin-left: 8%'>Sources</h1>
     <ion-item>
       <ion-grid>
         <ion-row>
-          <ion-col size="10">
-            <ion-input type="email" v-model='sourceUrl' placeholder='Correct source'></ion-input>
+          <ion-col size='10'>
+            <ion-input type='email' v-model='sourceUrl' placeholder='Source URL'></ion-input>
           </ion-col>
-          <ion-col size="2" class="add-source-icon">
-            <ion-icon @click="addSource"
-                      size="large"
-                      src="assets/icons/arrow-down-circle-outline.svg">
+          <ion-col size='2' class='add-source-icon'>
+            <ion-icon @click='addSource'
+                      size='large'
+                      src='assets/icons/arrow-down-circle-outline.svg'>
             </ion-icon>
           </ion-col>
         </ion-row>
       </ion-grid>
     </ion-item>
-    <ion-content :fullscreen="false" class='content'>
+    <ion-content :fullscreen='false' class='content'>
       <ion-grid>
-        <ion-row class='content'  :key="source.index" v-for="source in inDb">
+        <ion-row class='content'  :key='source.index' v-for='source in inDb'>
 
-          <ion-col size="7" class="source-name-container">
-            <div class="">
-              <p class="source-name">{{ source.name }}</p>
+          <ion-col size='7' class='source-name-container'>
+            <div>
+              <p class='source-name'>{{ source.name }}</p>
             </div>
           </ion-col>
 
-          <ion-col size="3" class="source-name-container">
-            <div class="">
-              <ion-toggle @ionChange="changeNotifications($event, source.id)" :checked="source.notifications"></ion-toggle>
+          <ion-col size='3' class='source-name-container'>
+            <div>
+              <ion-toggle @ionChange='changeNotifications($event, source.id)' :checked='source.notifications'></ion-toggle>
             </div>
           </ion-col>
 
-          <ion-col size="2" class="source-name-container">
-            <div class="">
-              <ion-icon size="large"
-                        src="assets/icons/close-outline.svg"
-                        @click="removeSource(source.id)">
+          <ion-col size='2' class='source-name-container'>
+            <div class=''>
+              <ion-icon size='large'
+                        src='assets/icons/close-outline.svg'
+                        @click='removeSource(source.id)'>
             </ion-icon>
             </div>
           </ion-col>
@@ -44,38 +44,31 @@
         </ion-row>
       </ion-grid>
     </ion-content>
-    <p style="margin-left: 8%; margin-top: 15%; margin-bottom: 15%">Please turn on notifications  in settings</p>
+    <p style='margin-left: 8%; margin-top: 15%; margin-bottom: 15%'>Please turn on notifications  in settings</p>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent,
-         IonPage,
-         IonLabel,
-         IonInput,
-         IonCol,
-         IonGrid,
-         IonIcon,
-         IonRow,
-         IonItem,
-         IonToggle} from '@ionic/vue';
+// Vue/Ionic
+import {
+  IonContent, IonPage, IonLabel, IonInput, IonCol,
+  IonGrid, IonIcon, IonRow, IonItem, IonToggle
+} from '@ionic/vue';
 import { defineComponent } from 'vue';
-  import { isRSS } from '@/services/helpers';
-  import { HTMLParser, sources, Options } from '@11me/xparse';
 
-  import session from '@/main';
+// NPM
+import { RSSParser, sources, Options } from '@11me/xparse';
+import { Http } from '@capacitor-community/http';
 
-  export default defineComponent({
-    components: {
-      IonContent,
-      IonPage,
-      IonCol,
-      IonIcon,
-      IonGrid,
-      IonRow,
-      IonToggle,
-      IonItem,
-      IonInput,
+// inner
+import { isRSS } from '@/services/helpers';
+import session from '@/main';
+import { useFeeds } from '../services/feeds.service';
+
+export default defineComponent({
+  components: {
+    IonContent, IonPage, IonCol, IonIcon,
+    IonGrid, IonRow, IonToggle, IonItem, IonInput,
   },
   data() {
     return {
@@ -83,11 +76,17 @@ import { defineComponent } from 'vue';
       sourceUrl: ''
     }
   },
+  setup() {
+    const { feeds, getFeeds } = useFeeds();
+    return {
+      feeds
+    }
+  },
   async created() {
     this.inDb = await session.query('SELECT * FROM trd_sources')
   },
   methods: {
-    async changeNotifications(change: any, sourceId: number) {
+    async changeNotifications(change: any, sourceId: number): Promise<void> {
        if (change.detail.checked) {
          await session.execute(`UPDATE trd_sources SET notifications=1 WHERE id=${sourceId}`);
        } else {
@@ -95,14 +94,40 @@ import { defineComponent } from 'vue';
        }
        this.inDb = await session.query('SELECT * FROM trd_sources');
     },
+
     async removeSource(sourceId: number) {
       await session.execute(`DELETE FROM trd_sources WHERE id=${sourceId}`);
       this.inDb = await session.query('SELECT * FROM trd_sources')
     },
-    async addSource() {
+
+    async addSource(): Promise<void> {
+      console.log(this.feeds)
+      async function fetchProvider(url: string) {
+        const resource = await Http.get({url})
+        return resource.data
+      }
+      const rssParser = new RSSParser(fetchProvider);
+
+      let rss = await rssParser.parse({
+        description: { 'url': this.sourceUrl, 'page_selector': '' },
+        options: {
+          title: {
+            selectors: ''
+          },
+          author: {
+            selectors: ''
+          },
+          pubDate: {
+            selectors: ''
+          },
+          content: {
+            selectors: ''
+          }
+        }
+      });
       let options = {
-        name: 'Bloomberg',
-        url: 'vc.ru',
+        name: rss[0].title[0],
+        url: this.sourceUrl,
         state: 1,
         notifications: 1,
         last_update: 100000
