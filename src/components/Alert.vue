199bfbc66bcba220b453b52c05d8732e1bff2581
nikopushkin <nikopushkin1@gmail.com>
2022-01-16 21:59:23 +0300

    add Alert component and service

---
+++ src/components/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+  <h4>{{ alert.alert }}</h4>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useAlert } from '@/services/alert.service';
+
+
+export default defineComponent({
+  name: 'AlertComponent',
+  setup() {
+    const alert = useAlert();
+    // const message = alert.value
+    return { alert }
+  }
+});
+</script>
